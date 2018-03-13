ad8b3093294e05418efd088c1f814704128d0b0d
leonardom249 <leonardo.megan@gmail.com>
2018-03-13 15:46:46 -0500

    completed challenge, though hesitant on .next() parts.  Everything seems to work via postman, not sure if I have the .next()'s correct though

--- tuesday-knex/knexfile.js
+++ tuesday-knex/knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/noteful-app',
+    connection: process.env.DATABASE_URL || 'postgres://yrxagrxf:I6u8YWwHWeCXERZ5kDuQJGBqIoxNisKf@nutty-custard-apple.db.elephantsql.com:5432/yrxagrxf',
     debug: true, // http://knexjs.org/#Installation-debug
     pool: {min : 1 , max : 2}
   },
--- tuesday-knex/routes/notes.router.js
+++ tuesday-knex/routes/notes.router.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const knex = require('../knex');
+
 const express = require('express');
 
 // Create an router instance (aka "mini-app")
@@ -23,12 +25,22 @@ router.get('/notes', (req, res, next) => {
     })
     .catch(err => next(err)); 
   */
+  //becomes:
+  knex('notes')
+    .select('id', 'title', 'content')
+    .where(function(){
+      if(searchTerm){
+        this.where('title', 'like',`%${searchTerm}%`);
+      }})
+    .then(list => {
+      res.json(list);
+    }).catch(err=>next(err));
 });
 
 /* ========== GET/READ SINGLE NOTES ========== */
 router.get('/notes/:id', (req, res, next) => {
   const noteId = req.params.id;
-
+  //match by noteId
   /*
   notes.find(noteId)
     .then(item => {
@@ -40,7 +52,24 @@ router.get('/notes/:id', (req, res, next) => {
     })
     .catch(err => next(err));
   */
+  knex('notes')
+    .select()
+    .where(function(){
+      if(noteId){
+        this.where('id', `${noteId}`);
+      }
+      else{
+        next();
+      }
+    })
+    .then(item => {
+      res.json(item);
+    })
+    .catch(err=>next(err));
 });
+///how do i get it to through the error?
+
+
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
@@ -73,6 +102,16 @@ router.put('/notes/:id', (req, res, next) => {
     })
     .catch(err => next(err));
   */
+
+  knex('notes')
+    .where(function(){
+      if(noteId){
+        this.where('id', `${noteId}`);
+      }
+    })
+    .update({title: `${updateObj.title}`, content: `${updateObj.content}`})
+    .then(item=>res.json(item))
+    .catch(err => next(err));
 });
 
 /* ========== POST/CREATE ITEM ========== */
@@ -96,6 +135,13 @@ router.post('/notes', (req, res, next) => {
     })
     .catch(err => next(err));
   */
+
+  knex('notes')
+    .insert(({title : `${newItem.title}`, content :`${newItem.content}`}))
+    .then(item => res.location(`http://${req.headers.host}/notes/${item.id}`)
+      .status(201).json(item)
+    )
+    .catch(err => next(err));
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
@@ -113,6 +159,10 @@ router.delete('/notes/:id', (req, res, next) => {
     })
     .catch(err => next(err));
   */
+  knex('notes')
+    .where('id', `${id}`)
+    .del()
+    .then(deleted => res.status(204).end());
 });
 
 module.exports = router;
