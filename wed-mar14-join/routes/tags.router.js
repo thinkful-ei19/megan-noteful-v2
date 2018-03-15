a6817ab70206cbca4a33dd267341ec7ed3b7f652
leonardom249 <leonardo.megan@gmail.com>
2018-03-15 14:06:13 -0500

    added all endpoints to tags.router.js

--- wed-mar14-join/routes/tags.router.js
+++ wed-mar14-join/routes/tags.router.js
@@ -4,9 +4,33 @@ const knex = require('../knex');
 const express = require('express');
 const router = express.Router();
 
+router.get('/tags', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('tags')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
 
+router.get('/tags/:id', (req, res, next) => {
+  const tagsId = req.params.id;
+  knex('tags')
+    .select()
+    .where(function(){
+      if(tagsId){
+        this.where('id', `${tagsId}`);
+      }
+      else{
+        next();
+      }
+    })
+    .then(item => {
+      res.json(item);
+    })
+    .catch(err=>next(err));
+});
 
-/* ========== POST/CREATE ITEM ========== */
 router.post('/tags', (req, res, next) => {
   const { name } = req.body;
   
@@ -28,5 +52,42 @@ router.post('/tags', (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.put('/tags/:id', (req, res, next) => {
+  const tagsId = req.params.id;
+  const updateObj = {};
+  const updateableFields = ['name'];
+    
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+    
+  if (!updateObj.name) {
+    const err = new Error('Missing `tag name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  
+  knex('tags')
+    .where(function(){
+      if(tagsId){
+        this.where('id', `${tagsId}`);
+      }
+    })
+    .update({name: `${updateObj.name}`})
+    .then(item=>res.json(item))
+    .catch(err => next(err));
+});
+
+router.delete('/tags/:id', (req, res, next) => {
+  const id = req.params.id;
+  
+  knex('tags')
+    .where('id', `${id}`)
+    .del()
+    .then(deleted => res.status(204).end());
+});
+  
 
 module.exports = router;
